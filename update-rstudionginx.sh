ef48d7817478ed2d3b14acc898a1d72b52a866aa
Jason Bramwell <jb2cool@gmail.com>
2025-07-10 17:51:33 +0100

    Implementing some updates suggested by Copilot

--- update-rstudionginx.sh
+++ update-rstudionginx.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 # Update R/RStudio Server/nginx on Ubuntu
 
+# Exit on error
+set -e
+
+# Logging function
+LOG_FILE="install-rstudioshinynginx.log"
+log() {
+    echo "[INFO] $1" | tee -a "$LOG_FILE"
+}
+log "Starting installation of R and nginx"
+
 # Update repository list and install R
+log "Installing latest version of R base"
 sudo apt-get update && sudo apt-get install r-base r-base-dev -y
 
 # Update RStudio Server
+log "Installing prerequisite package for RStudio Server"
 sudo apt-get install gdebi-core -y
+log "Downloading RStudio Server"
 if [[ $(lsb_release -rs) == "20.04" ]]
 then
     wget https://www.rstudio.org/download/latest/stable/server/focal/rstudio-server-latest-amd64.deb  -O rstudio-latest.deb
@@ -18,8 +31,14 @@ then
 else
     echo "Non-compatible version"
 fi
+log "Installing RStudio Server"
 sudo gdebi --non-interactive rstudio-latest.deb
+log "Cleaning up RStudio Server"
 rm rstudio-latest.deb
 
 # Update nginx
+log "Installing latest version of nginx"
 sudo apt-get install nginx -y
+
+# Clean up install log
+log "Cleaning up install log"
