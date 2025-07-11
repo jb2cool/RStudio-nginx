3df90a314892532824016c6416d990fbffeca5b2
Jason Bramwell <jb2cool@gmail.com>
2025-07-11 09:44:01 +0100

    re-adding logging

--- install-rstudionginx.sh
+++ install-rstudionginx.sh
@@ -4,7 +4,19 @@
 # Exit on error
 set -e
 
+# Setting up logging
+LOG_FILE="~/install-rstudionginx.log"
+touch "$LOG_FILE" || { echo "Cannot write to log file: $LOG_FILE"; exit 1; }
+log() {
+    local timestamp
+    timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+    echo "[$timestamp] $1" | tee -a "$LOG_FILE"
+}
+
+log "Starting installation of R, Shiny Server, and nginx..."
+
 # Add CRAN repository to APT sources
+log "Adding CRAN repository..."
 if [[ $(lsb_release -is) == "Ubuntu" ]]
 then
     echo "Linux distribution is Ubuntu, proceeding to add R repository to APT"
@@ -15,13 +27,17 @@ else
 fi
 
 # Add CRAN repository key
+log "Adding CRAN repository key..."
 wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 
 # Update repository list and install R
+log "Installing R base..."
 sudo apt-get update && sudo apt-get install r-base r-base-dev -y
 
 # Install RStudio Server
+log "Installing RStudio Server dependencies..."
 sudo apt-get install gdebi-core -y
+log "Downloading RStudio Server..."
 if [[ $(lsb_release -rs) == "20.04" ]]
 then
     wget https://www.rstudio.org/download/latest/stable/server/focal/rstudio-server-latest-amd64.deb  -O rstudio-latest.deb
@@ -34,22 +50,28 @@ then
 else
     echo "Non-compatible version"
 fi
+log "Installing RStudio Server..."
 sudo gdebi --non-interactive rstudio-latest.deb
 rm rstudio-latest.deb
 
 # Install nginx
+log "Installing nginx..."
 sudo apt-get install nginx -y
 
 # Configure nginx with RStudio Server redirect
+log "Downlkoading and installing pre-configured nginx config..."
 sudo wget https://raw.githubusercontent.com/jb2cool/RStudio-nginx/main/default -O /etc/nginx/sites-enabled/default
 
 # Restart services
+log "Reloading nginx service..."
 sudo systemctl reload nginx
 
 # Clean up install script
+log "Cleaning up install script..."
 rm install-rstudionginx.sh
 
 # Tell user everything works
+log "End of install output to user..."
 echo ""
 echo ""
 echo "###############################################################################"
@@ -57,3 +79,6 @@ echo "# RStudio Server is now available on http://127.0.0.1:8787 & http://127.0.
 echo "###############################################################################"
 echo ""
 echo ""
+
+# Cleaning up logging
+rm $LOG_FILE
